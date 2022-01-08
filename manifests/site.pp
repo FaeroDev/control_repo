751a1951ce60409646f982f557818cbf54501342
M. Scott Pawlak <faerodev@gmail.com>
2022-01-08 13:27:19 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,5 +1,14 @@
 node default{
+  file { '/root/README:
+    ensure  => file,
+    content => 'This is a readme'
+    owner   => 'root',
+  }
 }
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^web/  {
+  include role::app
 }
