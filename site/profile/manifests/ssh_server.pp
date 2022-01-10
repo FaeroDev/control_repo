089b601c752b1db12fb123a5cea1c6434dc74a0a
M. Scott Pawlak <faerodev@gmail.com>
2022-01-10 16:16:09 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
           ensure => present,
           user   => 'root',
           type   => 'ssh-rsa',
-          key    => '',
+          key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDTNaGRfWJJf7JKKr1TutCjXYirnL7VnRsBx9lQeeXcliHz8KafzrsGscmDEiyEp4mmrGSXpRU+cYfUHB6t5P1cNUhyWzc5VYYR2qjQ94kM7gOv37cI5e0CvNDQBc6GY/faoUcHvtpFf2MwdNxsCYxgCTlwQbN6U3c2DPn6fZydo/L5Z+8pX4irlMfmYqSFcBLkPj84od2VBaZ3bxXzU9l7ZTSiZClol/0YAP6QycdUIbX9bdPBo1UbTZmWstewF7GYaU6mQJjcw0ir71d/vvwX2bylskoXfvHKhGjzXVLlIbmHO1CTb0wcRxyzjqwax2cZ4v2oPyiqarabtX/Eb4lF',
    }
 }
